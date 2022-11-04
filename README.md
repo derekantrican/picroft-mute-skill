5a3fadd78b77bf66a6f14da8c529ab4441196364
Grocery 2 Webhook <derekantrican@gmail.com>
2022-11-03 22:46:09 -0700

    Implemented settings

--- README.md
+++ README.md
@@ -4,18 +4,12 @@ Adds support for a mute button to picroft
 ## About
 Adds support for a mute button to picroft using the gpio pins. You can either say one of the mute phrases (such as "Mycroft, mute" or "Mycroft, stop listening") or push the button to toggle mute.
 
-Currently, this skill is NOT customizable, but will be in the future. It is strictly set up for the below configuration:
-
-- Button: BOARD pin 10 (**NOTE: this mycroft skill is not set up for a regular push button, but for a [toggle button](https://www.adafruit.com/product/1683)**)
-- LED: BOARD pin 12
+To configure this skill go [to the skill config](https://account.mycroft.ai/skills)
 
 ![](https://i.imgur.com/Nge0pfE.png)
 
 ## Future plans for customization:
 
-- Pin number for the button
-- Pin number for the LED
-- Whether the LED "on" represents "muted" or "unmuted"
 - Whether the button is a simple push button or a toggle button
 
 ## Credits
--- __init__.py
+++ __init__.py
@@ -7,18 +7,40 @@ class PicroftMuteSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
+    muted = False
+
     def initialize(self):
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(12, GPIO.OUT) # set up LED pin # TODO: allow this pin # to be customized
-        GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # set up button event handler # TODO: allow this pin # to be customized
-        GPIO.add_event_detect(10, GPIO.BOTH, self.handle_button_press)
+        self.settings_change_callback = self.on_settings_changed
+        self.on_settings_changed(False)
 
     def shutdown(self):
         self.log.info("Performing shutdown....")
-        GPIO.remove_event_detect(10) # remove button event handler
-        GPIO.cleanup() # TODO: currently I don't think this is cleaning up previous event handlers
+        self.bus.emit(Message('mycroft.volume.unmute', data={"speak_message": False})) # Turn the volume back on if shutdown
+        self.cleanupGPIO()
 
-    muted = False
+    def on_settings_changed(self, doCleanup = True):
+        self.log.info(f"Settings changed! \n\t{self.settings}")
+
+        self.led_pin = self.settings.get('led_pin')
+        self.button_pin = self.settings.get('button_pin')
+
+        if doCleanup:
+            self.cleanupGPIO()
+
+        self.initGPIO()
+
+        self.led_mute_behavior = self.settings.get('led_mute_behavior')
+        self.set_led_state()
+
+    def initGPIO(self):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.led_pin, GPIO.OUT) # set up LED pin
+        GPIO.setup(self.button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # set up button event handler
+        GPIO.add_event_detect(self.button_pin, GPIO.BOTH, self.handle_button_press)
+
+    def cleanupGPIO(self):
+        GPIO.remove_event_detect(self.button_pin) # remove button event handler
+        GPIO.cleanup()
 
     @intent_file_handler('mute.intent')
     def handle_mute_picroft(self, message):
@@ -29,19 +51,26 @@ class PicroftMuteSkill(MycroftSkill):
             self.handle_mute()
 
     def handle_mute(self):
-        GPIO.output(12, GPIO.HIGH) # Turn on LED
+        self.set_led_state()            
         self.speak_dialog("muted")
         wait_while_speaking()
         self.bus.emit(Message('mycroft.volume.mute', data={"speak_message": False})) # This has to come after the speak_dialog line for the speak_dialog to be heard
 
     def handle_unmute(self):
-        GPIO.output(12, GPIO.LOW) # Turn off LED
+        self.set_led_state()
         self.speak_dialog("unmuted")
         self.bus.emit(Message('mycroft.volume.unmute', data={"speak_message": False}))
 
+    def set_led_state(self):
+        if ((self.muted and self.led_mute_behavior == 'muted') or
+           (not self.muted and self.led_mute_behavior == 'unmuted')):
+            GPIO.output(self.led_pin, GPIO.HIGH) # Turn on LED
+        else:
+            GPIO.output(self.led_pin, GPIO.LOW) # Turn off LED
+
     def handle_button_press(self, channel):
         self.log.info(f"handling button press (pin {channel}: {GPIO.input(channel)}). muted: {self.muted}")
-        # NOTE: this code assumes a "toggle button" right now and should be changed to also be compatible with a push button
+        # TODO: this code assumes a "toggle button" right now and should be changed to also be compatible with a push button
         if GPIO.input(channel) == GPIO.HIGH and not self.muted:
             self.muted = True
             self.handle_mute()
